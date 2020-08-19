a9fc53de3481162b636815fcce7cf0e9ab9ccc09
Larissa Gusmao <larissamgusmao@gmail.com>
2020-08-19 16:22:42 -0300

    Add Sidebar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBlPeAUst-6k43oZPbNnBLBiyNPEdgQ2b8",
+    authDomain: "whatsappclone-de3cd.firebaseapp.com",
+    databaseURL: "https://whatsappclone-de3cd.firebaseio.com",
+    projectId: "whatsappclone-de3cd",
+    storageBucket: "whatsappclone-de3cd.appspot.com",
+    messagingSenderId: "3726260076",
+    appId: "1:3726260076:web:106897909634fe535ea549",
+    measurementId: "G-GM8724TK6H"
+};
